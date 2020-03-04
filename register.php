8a5e666a1ee5f6529ae46a7ad90404676e47ccdf
Eric Mouse <mouseisnotalwaysmouse@outlook.com>
2020-03-04 14:30:25 +0800

    优化了注册逻辑

--- register.php
+++ register.php
@@ -1,9 +1,13 @@
 <?php
 	require_once 'php_function/db_con.php';
-	if(isset($_POST["email"]) && isset($_POST["passwd"]))
+	if(isset($_POST))
 	{
-		$uemail = $_POST["email"];
-		$upw = $_POST["passwd"];
+		 $json = file_get_contents('php://input');
+    	//加true转换为数组，不加转换为对象
+    	$arr =  json_decode($json, true);
+		//echo $_POST[0];
+		$uemail = $arr["email"];
+		$upw = $arr["passwd"];
 		$upw = md5($upw);
 		$ugroup = 2;
 
@@ -20,33 +24,6 @@
 							VALUES
 							("'.$uemail.'","'.$uname.'","'.$ugroup.'","'.$upw.'","1");';
 			$conn->query($sql_new_user);
-			
-			echo "<script>alert('Record Added');location.assign('mst_lst_user.php".$qr_string."');</script>";	
 		}
 	}
-	/*
-	if(isset($_GET["email"]) && isset($_GET["passwd"]))
-	{
-		$uemail = $_GET["email"];
-		$upw = $_GET["passwd"];
-		$upw = md5($upw);
-		$ugroup = 2;
-
-		$sql_user_exists = "SELECT * FROM users WHERE uemail = '$uemail'"; 
-		$result_user_exists = $conn->query($sql_user_exists);
-			
-		if ($result_user_exists->num_rows != 0){
-			echo '<script>
-				alert("Username already existed! Record doesn\'t saved");
-			</script>';
-		}
-		else{
-			$sql_new_user = 'INSERT INTO users (uemail,uname,ugroup,upw,ufirst_login)
-							VALUES
-							("'.$uemail.'","'.$uname.'","'.$ugroup.'","'.$upw.'","1");';
-			$conn->query($sql_new_user);
-			
-			echo "<script>alert('Record Added');location.assign('mst_lst_user.php".$qr_string."');</script>";	
-		}
-	}*/
 ?>
